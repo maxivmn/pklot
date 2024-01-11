63329c7a2c64dd428ac7eae232a00f9f2c8a1044
Margarita <ri_sa@hotmail.de>
2024-01-11 15:54:57 +0100

    Moving the data into right shape. Creating train, test folders. Dataloader + some trafos

---
+++ notebooks/config.py
@@ -0,0 +1,14 @@
+# specify path to dataset
+DATASET_PATH_RAW = "../data/PKLot/PKLotSegmented/PUC/Sunny/"
+DATASET_PATH = "../data/PKLot/PKLotSegmented_rearranged/PUC/Sunny/"
+# specify the paths to our training and validation set 
+TRAIN = "train"
+TEST = "test"
+VAL = "val"
+# set the input height and width
+INPUT_HEIGHT = 128
+INPUT_WIDTH = 128
+# set the batch size and validation data split
+BATCH_SIZE = 8
+VAL_SPLIT = 0.1
+TEST_SPLIT = 0.2
